18203562f37f583bd9f4f63f1d3a41ea45106100
Tiago <you@example.com>
2015-12-04 16:55:32 +0000

    Isto esta tudo mal

--- LexPars/Compile.hs
+++ LexPars/Compile.hs
@@ -1,10 +1,11 @@
 module Compile where
 import Grammar
+import Tokens
 
 type Code = (Op, Value, Value, Value)
 data Op =  OpIf | OpAt | OpLb
-data Value = Const Type | UVar String | Label String | TVar(String, Type) | Null deriving(Show)
-
+data Value = Exp | UVar String | Label String | TVar(String, Type) | Null deriving(Show)
+  -- aqui no value o Exp para ir buscar o Num Int???
 
 new_label :: Int -> String
 new_label n = "label" ++ (show n)
@@ -14,11 +15,13 @@ new_var n = "$t" ++ (show n)
 
 
 compile_cmd :: Int -> Command -> ([Code], Int)
-compile_cmd :: label (Atrib (SVar var) exp) = (expCode ++ [(OpAt, UVar var, UVar expVar, Null)], label)
+compile_cmd :: lbNum (Atrib (SVar var) exp) = (expCode ++ [(OpAt, UVar var, UVar expVar, Null)], lbNum)
             where (expVar, expCode) = compile_exp 0 exp
+            -- este lbNum, onde e que isto vai ser metido???????
+
 
 compile_exp :: Int -> Exp -> (String, [Code], Int)
-compile_exp nx (Num x) = (var, [(OpAt, UVar var, Const (Num x), Null)], nx + 1)
+compile_exp nx (Num x) = (var, [(OpAt, UVar var, (Num x), Null)], nx + 1)
             where var = new_var nx
 
 mipscode:: Code -> String
